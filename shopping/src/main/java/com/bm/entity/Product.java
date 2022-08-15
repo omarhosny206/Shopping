ddeb8655317848673a88fce255e8e2f830af9459
omarhosny206 <omarhosny102@gmail.com>
2022-08-15 07:16:40 +0200

    Added Product entity

---
+++ shopping/src/main/java/com/bm/entity/Product.java
@@ -0,0 +1,24 @@
+package com.bm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int size;
+    private String image;
+}
