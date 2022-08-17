dfcad3ade7ecde17099f5af75e66d62bc0ce50db
omarhosny206 <omarhosny102@gmail.com>
2022-08-17 13:12:45 +0200

    Added logger to CategoryService

--- src/main/java/com/bm/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/bm/service/impl/CategoryServiceImpl.java
@@ -3,9 +3,11 @@ package com.bm.service.impl;
 import com.bm.entity.Category;
 import com.bm.repository.CategoryRepository;
 import com.bm.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
@@ -15,11 +17,13 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category findByName(String categoryName) {
+        log.info("Getting category with name={}", categoryName);
         return categoryRepository.findByName(categoryName);
     }
 
     @Override
     public String save(Category category) {
+        log.info("Saving category with name={}", category.getName());
         categoryRepository.save(category);
         return "Added successfully";
     }
