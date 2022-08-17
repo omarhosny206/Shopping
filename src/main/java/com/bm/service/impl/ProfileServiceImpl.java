7330890f9f9d434352b891961bf40dc4eaf58b89
omarhosny206 <omarhosny102@gmail.com>
2022-08-17 13:28:32 +0200

    Added logger to ProfileService

--- src/main/java/com/bm/service/impl/ProfileServiceImpl.java
+++ src/main/java/com/bm/service/impl/ProfileServiceImpl.java
@@ -5,6 +5,7 @@ import com.bm.entity.User;
 import com.bm.service.ProfileService;
 import com.bm.service.UserService;
 import com.bm.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class ProfileServiceImpl implements ProfileService {
     private final UserService userService;
     private final JwtUtil jwtUtil;
@@ -26,19 +28,29 @@ public class ProfileServiceImpl implements ProfileService {
 
     @Override
     public ResponseEntity<?> update(UpdateRequest updateRequest, String header) {
-        if(header == null || !header.startsWith("Bearer"))
+        log.info("Trying to update user info");
+
+        if(header == null || !header.startsWith("Bearer")) {
+            log.error("Bad Request, empty or incorrect header");
             return new ResponseEntity<>(new ErrorMessage("Bad Request"), HttpStatus.BAD_REQUEST);
+        }
+
 
         String jwt = header.substring(7);
         String email= jwtUtil.getUserName(jwt);
         User user = userService.getByEmail(email);
 
-        if(user == null)
+        if(user == null) {
+            log.error("User not found");
             return new ResponseEntity<>(new ErrorMessage("Incorrect user"), HttpStatus.NOT_FOUND);
+        }
+
 
         user.setName(updateRequest.getName());
         user.setPassword(passwordEncoder.encode(updateRequest.getPassword()));
         userService.save(user);
+
+        log.info("User updated successfully");
         return new ResponseEntity<>("Updated successfully", HttpStatus.OK);
     }
 }
