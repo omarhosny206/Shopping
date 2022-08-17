0e607adb4506dd9d60126ff03befd8cf8d434f66
omarhosny206 <omarhosny102@gmail.com>
2022-08-17 13:08:41 +0200

    Added logger for ProductController

--- src/main/java/com/bm/controller/ProductController.java
+++ src/main/java/com/bm/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.bm.controller;
 import com.bm.dto.ProductDto;
 import com.bm.entity.Product;
 import com.bm.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@Slf4j
 public class ProductController {
     private final ProductService productService;
 
@@ -21,25 +23,32 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<?> getAllByCategoryName(@RequestParam String category) {
+        log.info("Getting all products with category-name={}", category);
         List<Product> products = productService.getAllByCategory(category);
 
-        if (products != null)
+        if (products != null) {
+            log.info("found successfully");
             return new ResponseEntity<>(products, HttpStatus.OK);
+        }
 
+        log.error("not found");
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     public Product getById(Long id) {
+        log.info("Getting product with id={}", id);
         return productService.getById(id);
     }
 
     @PostMapping("/")
     public ResponseEntity<String> save(@RequestBody ProductDto productDto) {
+        log.info("Saving a new product={}", productDto);
         return new ResponseEntity<>(productService.save(productDto), HttpStatus.OK);
     }
 
     @DeleteMapping("")
     public ResponseEntity<String> deleteById(@RequestParam Long id) {
+        log.info("Deleting a product with id={}", id);
         return new ResponseEntity<>(productService.deletyById(id), HttpStatus.OK);
     }
 }
