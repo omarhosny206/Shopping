ef19de8e6d8099582f6993f5f6f8e019f9df21e1
omarhosny206 <omarhosny102@gmail.com>
2022-08-17 13:30:26 +0200

    Added logger to RegistrationService

--- src/main/java/com/bm/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/bm/service/impl/RegistrationServiceImpl.java
@@ -6,12 +6,14 @@ import com.bm.exception.Errors;
 
 import com.bm.service.RegistrationService;
 import com.bm.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class RegistrationServiceImpl implements RegistrationService {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
@@ -23,12 +25,15 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public ResponseEntity<?> register(User user) {
+        log.info("Trying to register a new user");
     	User possibleUser = userService.getByEmail(user.getEmail());
     	
     	if(possibleUser != null) {
+            log.error("This email is already taken");
     		return new ResponseEntity<>(new ErrorResponse(Errors.EmailNotUnique.getErrorMessage()), HttpStatus.BAD_REQUEST);
     	}
 
+        log.error("User registered successfully");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.save(user);
         return new ResponseEntity<>("Registered Successfully", HttpStatus.OK);
